f7e70cf29102f50c1d09fc14b78ab83383ad871f
Richard John <richard@dominorecordco.com>
2018-02-05 15:07:12 +0000

    Added dashboard template

---
+++ resources/views/index.blade.php
@@ -0,0 +1,21 @@
+{{-- resources/views/admin/dashboard.blade.php --}}
+
+@extends('adminlte::page')
+
+@section('title', 'UNILAD Insights')
+
+@section('content_header')
+    <h1>UNILAD Insights Dashboard</h1>
+@stop
+
+@section('content')
+    <p>Welcome to the UNILAD insights dashboard.</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> </script>
+@stop
