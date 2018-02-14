2cf33beb87c77843f3e8e14d8f94cedab1805b45
Richard John <richard@dominorecordco.com>
2018-02-14 11:05:46 +0000

    Linking posts to pages

--- app/Console/Commands/GetPagePosts.php
+++ app/Console/Commands/GetPagePosts.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use Facebook\Facebook;
 use App\Post;
+use App\Page;
 
 class GetPagePosts extends Command
 {
@@ -45,7 +46,8 @@ class GetPagePosts extends Command
             $postResponse = $api->get('/' . $node->getField('id') . '?fields=message,name,link,picture,type,created_time', env('FACEBOOK_ACCESS_TOKEN'));
             $postId = explode("_", $postResponse->getGraphNode()->getField('id'))[1];
             $post = Post::firstOrNew(['facebook_id' => $postId]);
-            $post->page_id = 0;
+            $page = Page::where('facebook_id', $this->argument('pageid'))->firstOrFail();            
+            $post->page_id = $page->id;
             $post->message = $postResponse->getGraphNode()->getField('message');
             $post->name = $postResponse->getGraphNode()->getField('name');
             $post->link = $postResponse->getGraphNode()->getField('link');
