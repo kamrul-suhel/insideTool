b827bff6843272bd12564b54ea54bc99803b3039
Richard John <richard@dominorecordco.com>
2018-02-20 16:04:59 +0000

    Added Envoy deploy, and added auth to interface

--- routes/web.php
+++ routes/web.php
@@ -11,15 +11,34 @@
 |
 */
 
-Route::get('/', function () {
-    return view('index');
+
+Route::middleware('auth')->group(function() {
+    Route::get('/', function () {
+        return view('index');
+    });
+
+    Route::get('/home', function () {
+        return view('index');
+    })->name('home');
+    
+    Route::get('/pages', 'PageController@index');
+    Route::bind('post', function($id, $route) {
+        return \App\Post::withTrashed()->find($id);
+    });
+    Route::get('/posts', 'PostController@index');
+    Route::get('/posts/{post}', 'PostController@show');
+    Route::get('/posts/{post}/snapshots/{type}/{metric}/{birth?}', 'PostController@jsonSnapshots')
+        ->where('metric', '(all|likes|loves|wows|hahas|sads|angrys|shares|comments)');
+    
 });
 
-Route::get('/pages', 'PageController@index');
-Route::bind('post', function($id, $route) {
-    return \App\Post::withTrashed()->find($id);
-});
-Route::get('/posts', 'PostController@index');
-Route::get('/posts/{post}', 'PostController@show');
-Route::get('/posts/{post}/snapshots/{type}/{metric}/{birth?}', 'PostController@jsonSnapshots')
-    ->where('metric', '(all|likes|loves|wows|hahas|sads|angrys|shares|comments)');
+// Authentication Routes...
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Password Reset Routes...
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
