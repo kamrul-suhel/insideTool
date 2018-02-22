9541e92c69a00be6fba2d6720ea4807836fb42c5
Richard John <richard@dominorecordco.com>
2018-02-22 16:35:46 +0000

    Added posted date to table

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -10,6 +10,7 @@
                 <thead>
                     <tr>
                         <th>Page</th>
+                        <th>Posted</th>
                         <th>Type</th>
                         <th></th>
                         <th>Message</th>
@@ -24,6 +25,7 @@
                         @endif
                         >
                             <td><strong>{{ $post->page->name }}</strong></td>
+                            <td>{{ date("d/m/Y H:i:s", strtotime($post->posted)) }}
                             <td>{{ title_case($post->type) }}</td>
                             <td><a href="/posts/{{ $post->id }}"><img src="{{ $post->picture }}" width="50"></a></td>
                             <td><a href="/posts/{{ $post->id }}">{{ $post->message }}</a></td>
