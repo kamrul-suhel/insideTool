57fc8eba0853fbd2a0811994911c0cd3150ae053
Richard John <richard@dominorecordco.com>
2018-02-28 17:42:28 +0000

    Try copy

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -78,7 +78,7 @@
 	ln -s {{ $path }}/storage {{ $release }}/storage
 	ln -s {{ $path }}/storage/public {{ $release }}/public/storage
 	echo "Storage directories set up"
-	ln -s {{ $path }}/.env {{ $release }}/.env
+	cp -f {{ $path }}/.env {{ $release }}/.env
 	echo "Environment file set up"
 @endtask
 
