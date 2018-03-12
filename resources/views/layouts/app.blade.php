8acac8a785d26ad4d623167f28ff39290f59690c
Richard John <richard@dominorecordco.com>
2018-03-12 14:46:29 +0000

    Made non-active label text more readable

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
 @section('css')
     <meta name="csrf-token" content="{{ csrf_token() }}">
     
-    <link rel="stylesheet" href="/css/admin_custom.css?201803121145">
+    <link rel="stylesheet" href="/css/admin_custom.css?201803121445">
 @stop
 
 @section('js')
