5ce72756d9f5fcdd97d5aeae8ffc7c61d49dd0aa
Richard John <richard@dominorecordco.com>
2018-03-22 14:18:41 +0000

    Fix filters

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -58,7 +58,9 @@ class PostController extends Controller
             $iaFilter = true;
         }
         
-        $posts->whereDate('posted', \Carbon\Carbon::now('Europe/London')->subDays($day)->format('Y-m-d'));
+        if (!$label && !$creator && $instantArticles) {
+            $posts->whereDate('posted', \Carbon\Carbon::now('Europe/London')->subDays($day)->format('Y-m-d'));
+        }
 
         $paginationLinks = [];
         $paginationLinks["days"] = [];
