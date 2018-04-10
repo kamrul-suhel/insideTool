ddc94971e068d2a30a0998c77196d4262893d5d9
Richard John <richard.john@gmail.com>
2018-04-10 16:03:59 +0100

    Go back to standard jQuery styles

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -248,9 +248,9 @@
 
 @push('js')
     <script src="{{ asset('js/app.js') }}"></script>
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs/dt-1.10.16/datatables.min.css"/>
-    <script type="text/javascript" src="https://cdn.datatables.net/v/bs/dt-1.10.16/datatables.min.js"></script>
-     <script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.16/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.16/js/dataTables.bootstrap.min.js"></script>
+    <script>
         $('#posts-table').dataTable({
             "paging": false
         });
