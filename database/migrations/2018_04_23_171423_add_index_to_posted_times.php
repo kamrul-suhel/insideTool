438bff48f486eccbb16eeea6187e7cca82f5b6c1
Richard John <richard.john@gmail.com>
2018-04-23 18:19:27 +0100

    Index posted times'

---
+++ database/migrations/2018_04_23_171423_add_index_to_posted_times.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIndexToPostedTimes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dateTime('posted')->index('posted')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dateTime('posted')->dropIndex('posted');
+        });
+    }
+}
