331cc36b2dd525c210f31d8e5fd866dce6b71f10
Richard John <richard.john@gmail.com>
2018-04-23 17:39:43 +0100

    Cache busting

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
 
 @section('css')
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="stylesheet" href="/css/admin_custom.css?201803121609">
+    <link rel="stylesheet" href="/css/admin_custom.css?201804241739">
 @stop
 
 @section('js')
