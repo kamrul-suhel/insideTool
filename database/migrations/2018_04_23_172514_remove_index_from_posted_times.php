992a42c3ee0b1689332e9e628817860b9c1214b8
Richard John <richard.john@gmail.com>
2018-04-23 18:27:08 +0100

    Added change()

--- database/migrations/2018_04_23_172514_remove_index_from_posted_times.php
+++ database/migrations/2018_04_23_172514_remove_index_from_posted_times.php
@@ -14,7 +14,7 @@ class RemoveIndexFromPostedTimes extends Migration
     public function up()
     {
         Schema::table('posts', function (Blueprint $table) {
-            $table->dateTime('posted')->dropIndex('posted');
+            $table->dateTime('posted')->dropIndex('posted')->change();
         });
     }
 
