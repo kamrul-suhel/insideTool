d821b5073caa05eb23fb6f3e09d568ca89a9fdfd
Hemm <hemmit.kerrai@unilad.co.uk>
2018-09-21 12:36:36 +0100

    remove video without permissions

--- app/Console/Commands/FB/GetVideoPageLengths.php
+++ app/Console/Commands/FB/GetVideoPageLengths.php
@@ -49,7 +49,7 @@ class GetVideoPageLengths extends Command
         $page = Page::where(['facebook_id' => $this->argument('pageid')])->first();
 
         $snapshotsWithRetention = VideoStatSnapshot::whereNotNull('total_video_retention_graph')->groupBy('post_id')->pluck('post_id');
-        $videos = Post::where('type', 'video')->where('length', '=', 0.00)->whereNull('delete_at')->whereIn('id', $snapshotsWithRetention)->get();
+        $videos = Post::where('type', 'video')->where('length', '=', 0.00)->whereNot('facebook_id', 1215858231887003)->whereNull('delete_at')->whereIn('id', $snapshotsWithRetention)->get();
 
         foreach($videos as $video) {
 	        $videoLengthResponse = $api->get('/' . $video->facebook_id . '/?fields=length', $page->access_token);
